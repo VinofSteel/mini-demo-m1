d7a243062d116ed8671818d19228aa8cbea7e6bd
Vinicius <v.nascimento23@yahoo.com.br>
2022-09-04 19:24:19 -0300

    first commit

---
+++ src/scripts/database.js
@@ -0,0 +1,58 @@
+const database = [
+    {
+        id: 0,
+        nome: "God of War",
+        imagem: "https://cdn1.epicgames.com/offer/3ddd6a590da64e3686042d108968a6b2/EGS_GodofWar_SantaMonicaStudio_S2_1200x1600-fbdf3cbc2980749091d52751ffabb7b7_1200x1600-fbdf3cbc2980749091d52751ffabb7b7",
+        categoria: "Ação",
+        preco: 250.50
+    },
+    {
+        id: 1,
+        nome: "Super Mario Oddisey",
+        imagem: "https://img.elo7.com.br/product/original/40FD7D3/painel-2x1m-super-mario-odyssey-decoracoes.jpg",
+        categoria: "Aventura",
+        preco: 300.00
+    },
+    {
+        id: 2,
+        nome: "The Last of Us Part II",
+        imagem: "https://upload.wikimedia.org/wikipedia/pt/9/96/The_Last_of_Us_2_capa.png",
+        categoria: "Ação",
+        preco: 259.90
+    },
+    {
+        id: 3,
+        nome: "Bloodborne",
+        imagem:"https://image.api.playstation.com/vulcan/img/rnd/202010/2614/Sy5e8DmeKIJVjlAGraPAJYkT.png",
+        categoria: "RPG",
+        preco: 150.00
+    },
+    {
+        id: 4,
+        nome: "FIFA 2022",
+        imagem: "https://cdn1.epicgames.com/offer/f5deacee017b4b109476933f7dd2edbd/EGS_EASPORTSFIFA23StandardEdition_EACanada_S2_1200x1600-c806355d9cc8b35ebe392f2a7db03075",
+        categoria: "Esportes",
+        preco: 350.00
+    },
+    {
+        id: 5,
+        nome: "Gran Turismo 7",
+        imagem: "https://image.api.playstation.com/vulcan/ap/rnd/202109/1321/eFGBuaRr21HUpGtsy3biwJip.png",
+        categoria: "Esportes",
+        preco: 349.90
+    },
+    {
+        id: 6,
+        nome: "Elden Ring",
+        imagem: "https://image.api.playstation.com/vulcan/ap/rnd/202110/2000/aGhopp3MHppi7kooGE2Dtt8C.png",
+        categoria: "RPG",
+        preco: 350.00
+    },
+    {
+        id: 7,
+        nome:"Marvel's Spider-Man Remastered",
+        imagem: "https://cdn1.epicgames.com/offer/4bc43145bb8245a5b5cc9ea262ffbe0e/EGS_MarvelsSpiderManRemastered_InsomniacGamesNixxesSoftware_S1_2560x1440-73702d11161b29a0b7c40a8b489b1808",
+        categoria: "Aventura",
+        preco: 350.00
+    }
+]
